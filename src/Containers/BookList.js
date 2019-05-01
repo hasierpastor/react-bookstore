6aece36ee4515d348f6b8e5eb8884ea12b1be706
Hasier Pastor <hasier.pastor@gmail.com>
2019-05-01 14:27:27 -0700

    added ADD and REMOVE tp root reducer

--- src/Containers/BookList.js
+++ src/Containers/BookList.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
-import Book from './Book';
-import AddBookForm from './AddBookForm';
-const uuidv4 = require('uuid/v1');
+import Book from '../Components/Book';
+import AddBookForm from '../Components/AddBookForm';
+// const uuidv4 = require('uuid/v1');
 
 class BookList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      books: [
-        {
-          iban: uuidv4(),
-          name: 'The Effective Engineer',
-          author: 'Edmond Lau'
-        },
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     books: [
+  //       {
+  //         iban: uuidv4(),
+  //         name: 'The Effective Engineer',
+  //         author: 'Edmond Lau'
+  //       },
 
-        { iban: uuidv4(), name: 'Salt Fat Acid Heat', author: 'Samin Nosrat' },
-        {
-          iban: uuidv4(),
-          name: 'Marina',
-          author: 'Carlos Ruiz Zafon'
-        }
-      ]
-    };
-  }
+  //       { iban: uuidv4(), name: 'Salt Fat Acid Heat', author: 'Samin Nosrat' },
+  //       {
+  //         iban: uuidv4(),
+  //         name: 'Marina',
+  //         author: 'Carlos Ruiz Zafon'
+  //       }
+  //     ]
+  //   };
+  // }
 
   addBook = book => {
     book.iban = uuidv4();
---
+++ src/rootReducer.js
@@ -0,0 +1,20 @@
+import { ADD, REMOVE } from './actionTypes';
+const uuidv4 = require('uuid/v4');
+
+const INITIAL_STATE = { books: [] };
+
+function rootReducer(state = INITIAL_STATE, action) {
+  if (action.type === ADD) {
+    action.book.iban = uuidv4();
+    return { books: [...state.books, action.book] };
+  }
+  if (action.type === REMOVE) {
+    let filteredBooks = state.books.filter(book => {
+      return action.name !== book.name;
+    });
+    return { books: filteredBooks };
+  }
+  return state;
+}
+
+export default rootReducer;
